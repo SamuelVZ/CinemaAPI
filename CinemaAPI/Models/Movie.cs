5d9ac2a11b022829ba0ce48ffad73b907d7064e8
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-07 13:50:11 -0800

    Movie model added

---
+++ CinemaAPI/Models/Movie.cs
@@ -0,0 +1,7 @@
+﻿namespace CinemaAPI.Models {
+    public class Movie {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Language { get; set; }
+    }
+}
