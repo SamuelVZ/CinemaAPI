115a50adfb946f677a7ffd00b362622f42f84142
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-07 14:04:05 -0800

    Movies controller added

---
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using CinemaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaAPI.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : Controller {
+
+        List<Movie> movies = new List<Movie> {
+            new Movie(){ Id = 0, Name = "Interestellar", Language = "English"},
+            new Movie(){ Id = 1, Name = "MadMax", Language = "English"},
+        };
+        [HttpGet]
+        public IActionResult Index() {
+            return Ok(movies);
+        }
+
+        [HttpPost] 
+        public IActionResult Add([FromBody]Movie movie) {
+            movies.Add(movie);
+            return Ok(movies);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Put([FromRoute]int id, [FromBody]Movie movie) {
+            movies[id] = movie;
+            return Ok(movies);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute]int id) {
+            movies.RemoveAt(id);
+            return Ok(movies);
+        }
+    }
+}
