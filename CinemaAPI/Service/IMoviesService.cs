7910c16a473cb8511e60a0afd3f5f8fa3e781916
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-08 15:27:41 -0800

    MovieService with add, getall anf get added

---
+++ CinemaAPI/Service/IMoviesService.cs
@@ -0,0 +1,11 @@
+﻿using CinemaAPI.Dto;
+using CinemaAPI.Models;
+
+namespace CinemaAPI.Service {
+    public interface IMoviesService {
+
+        public List<Movie> GetAllMovies();
+        public Movie GetMovieById(int id);
+        public MovieDto AddMovie(AddMovieDto addMoviedto);
+    }
+}
---
+++ CinemaAPI/Service/MoviesService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CinemaAPI.Dto;
+using CinemaAPI.Models;
+using CinemaAPI.Repository;
+
+namespace CinemaAPI.Service {
+    public class MoviesService : IMoviesService {
+        private readonly IMoviesRepository _moviesRepository;
+        private readonly IMapper _mapper;
+
+        public MoviesService(IMoviesRepository moviesRepository, IMapper mapper) {
+            this._moviesRepository = moviesRepository;
+            this._mapper = mapper;
+        }
+
+        public MovieDto AddMovie(AddMovieDto addMoviedto) {
+            Movie movie = _mapper.Map<AddMovieDto, Movie>(addMoviedto);
+
+            Movie addedMovie =  _moviesRepository.AddMovie(movie);
+
+            MovieDto addedMovieDto = _mapper.Map<Movie, MovieDto>(addedMovie);
+
+            return addedMovieDto;
+        }
+
+        public List<Movie> GetAllMovies() {
+            return _moviesRepository.GetAllMovies();
+        }
+
+        public Movie GetMovieById(int id) {
+            return _moviesRepository.GetMovieById(id);
+        }
+    }
+}
