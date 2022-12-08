acdfa669e26e54f22fd3948a4e7a3be69ffb967c
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-08 15:26:03 -0800

    Get, GetById & Post methods updated to DBcontext

--- CinemaAPI/Controllers/MoviesController.cs
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
-﻿using CinemaAPI.Models;
+﻿using CinemaAPI.Dto;
+using CinemaAPI.Models;
+using CinemaAPI.Service;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CinemaAPI.Controllers {
@@ -6,33 +8,46 @@ namespace CinemaAPI.Controllers {
     [Route("api/[controller]")]
     public class MoviesController : Controller {
 
-        List<Movie> movies = new List<Movie> {
-            new Movie(){ Id = 0, Name = "Interestellar", Language = "English"},
-            new Movie(){ Id = 1, Name = "MadMax", Language = "English"},
-        };
+        private readonly IMoviesService _moviesService;
+
+        public MoviesController(IMoviesService moviesService) {
+            this._moviesService = moviesService;
+        }
+
+        //List<Movie> movies = new List<Movie> {
+        //    new Movie(){ Id = 0, Name = "Interestellar", Language = "English"},
+        //    new Movie(){ Id = 1, Name = "MadMax", Language = "English"},
+        //};
+
         [HttpGet]
-        public IActionResult Index() {
-            return Ok(movies);
+        public IActionResult GetAll() {
+            return Ok(_moviesService.GetAllMovies());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetMovieById([FromRoute]int id) {
+            return Ok(_moviesService.GetMovieById(id));
         }
 
         [HttpPost] 
-        public IActionResult Add([FromBody]Movie movie) {
-            movies.Add(movie);
-            return Ok(movies);
+        public IActionResult Add([FromBody]AddMovieDto addMovieDto) {
+            var movie = _moviesService.AddMovie(addMovieDto);
+            return Ok(movie);
         }
 
-        [HttpPut]
-        [Route("{id}")]
-        public IActionResult Put([FromRoute]int id, [FromBody]Movie movie) {
-            movies[id] = movie;
-            return Ok(movies);
-        }
+        //[HttpPut]
+        //[Route("{id}")]
+        //public IActionResult Put([FromRoute]int id, [FromBody]Movie movie) {
+        //    movies[id] = movie;
+        //    return Ok(movies);
+        //}
 
-        [HttpDelete]
-        [Route("{id}")]
-        public IActionResult Delete([FromRoute]int id) {
-            movies.RemoveAt(id);
-            return Ok(movies);
-        }
+        //[HttpDelete]
+        //[Route("{id}")]
+        //public IActionResult Delete([FromRoute]int id) {
+        //    movies.RemoveAt(id);
+        //    return Ok(movies);
+        //}
     }
 }
