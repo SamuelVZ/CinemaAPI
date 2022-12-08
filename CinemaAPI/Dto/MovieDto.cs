5b78157bece3470e6c9133c2e2c5830e36fc7a4d
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-08 15:23:32 -0800

    MovieDto added

---
+++ CinemaAPI/Dto/MovieDto.cs
@@ -0,0 +1,7 @@
+﻿namespace CinemaAPI.Dto {
+    public class MovieDto {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Language { get; set; }
+    }
+}
