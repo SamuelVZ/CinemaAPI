6a6373a21be91b1a6f99b245fb08eae32b737bff
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-08 15:21:48 -0800

    DbContext injected, scope added for interfaces and initial DB check

--- CinemaAPI/Program.cs
+++ CinemaAPI/Program.cs
@@ -1,3 +1,11 @@
+using CinemaAPI.Data;
+using CinemaAPI.Repository;
+using CinemaAPI.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using MySql.EntityFrameworkCore.Extensions;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,12 +15,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//inject DB connection service
+builder.Services.AddDbContext<CinemaDbContext>(options => {
+    options.UseMySQL(builder.Configuration.GetConnectionString("CinemaDB"));
+});
+
+
+//Add scope for interfaces (when you call the interface then the class is implemented instead)
+builder.Services.AddScoped<IMoviesRepository, MoviesRepository>();
+builder.Services.AddScoped<IMoviesService, MoviesService>();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope()) {
+    var dbContext = scope.ServiceProvider.GetRequiredService<CinemaDbContext>();
+    dbContext.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
@@ -22,3 +47,12 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+//public class MysqlEntityFrameworkDesignTimeServices : IDesignTimeServices {
+//    public void ConfigureDesignTimeServices(IServiceCollection serviceCollection) {
+//        serviceCollection.AddEntityFrameworkMySQL();
+//        new EntityFrameworkRelationalDesignServicesBuilder(serviceCollection)
+//            .TryAddCoreServices();
+//    }
+//}
