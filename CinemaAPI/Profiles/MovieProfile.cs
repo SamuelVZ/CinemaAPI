956b824516c8692092b95ae1cec56b671521156c
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-08 15:24:13 -0800

    Movie profie for mapping added

---
+++ CinemaAPI/Profiles/MovieProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using CinemaAPI.Dto;
+using CinemaAPI.Models;
+
+namespace CinemaAPI.Profiles {
+    public class MovieProfile : Profile {
+        public MovieProfile() {
+            CreateMap<Movie, MovieDto>().ReverseMap();
+            CreateMap<Movie, AddMovieDto>().ReverseMap();
+        }
+    }
+}
