9c1344cea042325afb57eec278b724d3cc5e4ca2
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-09 08:26:28 -0800

    Movies Dtos updated

--- CinemaAPI/Dto/AddMovieDto.cs
+++ CinemaAPI/Dto/AddMovieDto.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CinemaAPI.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CinemaAPI.Dto {
     public class AddMovieDto {
@@ -6,9 +8,21 @@ namespace CinemaAPI.Dto {
         //no need for this on .net 6 fields since default is required unless you specify the prop can be nullable with "?"
         //only use it if you want to display a personalkized messaje for empty values
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Language { get; set; }
-        public double Rate { get; set; }
-        public IFormFile? File { get; set; }
-        public string? ImageUrl { get; set; }
+        public string Duration { get; set; }
+        public DateTime PlayingDate { get; set; }
+        public DateTime PlayingTime { get; set; }
+        public double TicketPrice { get; set; }
+        public double Rating { get; set; }
+        public string Genre { get; set; }
+        public string TrailerUrl { get; set; }
+        public string ImageUrl { get; set; }
+
+
+        [NotMapped] //not maped does not in clude the field in to the DB
+        public IFormFile File { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; }
     }
 }
--- CinemaAPI/Dto/MovieDto.cs
+++ CinemaAPI/Dto/MovieDto.cs
@@ -1,10 +1,25 @@
-﻿namespace CinemaAPI.Dto {
+﻿using CinemaAPI.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CinemaAPI.Dto {
     public class MovieDto {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Language { get; set; }
-        public double Rate { get; set; }
-        public IFormFile File { get; set; }
+        public string Duration { get; set; }
+        public DateTime PlayingDate { get; set; }
+        public DateTime PlayingTime { get; set; }
+        public double TicketPrice { get; set; }
+        public double Rating { get; set; }
+        public string Genre { get; set; }
+        public string TrailerUrl { get; set; }
         public string ImageUrl { get; set; }
+
+
+        [NotMapped] //not maped does not in clude the field in to the DB
+        public IFormFile File { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; }
     }
 }
