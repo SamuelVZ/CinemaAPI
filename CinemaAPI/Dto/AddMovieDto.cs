3c6db55540935339c986ee5bc234ebe3ed336327
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-09 07:02:49 -0800

    Dtos updated to include images fields

--- CinemaAPI/Dto/AddMovieDto.cs
+++ CinemaAPI/Dto/AddMovieDto.cs
@@ -2,5 +2,8 @@
     public class AddMovieDto {
         public string Name { get; set; }
         public string Language { get; set; }
+        public double Rate { get; set; }
+        public IFormFile File { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
--- CinemaAPI/Dto/MovieDto.cs
+++ CinemaAPI/Dto/MovieDto.cs
@@ -3,5 +3,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Language { get; set; }
+        public double Rate { get; set; }
+        public IFormFile File { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
