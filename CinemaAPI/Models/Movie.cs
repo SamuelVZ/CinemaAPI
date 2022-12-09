6760c789ab723ba6399071dbae8cb44c5ec8dec0
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-09 08:08:40 -0800

    movie model updated

--- CinemaAPI/Models/Movie.cs
+++ CinemaAPI/Models/Movie.cs
@@ -4,13 +4,22 @@ namespace CinemaAPI.Models {
     public class Movie {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Language { get; set; }
-        public double Rate { get; set; }
+        public string Duration { get; set; }
+        public DateTime PlayingDate { get; set; }
+        public DateTime PlayingTime { get; set; }
+        public double TicketPrice { get; set; }
+        public double Rating { get; set; }
+        public string Genre { get; set; }
+        public string TrailerUrl { get; set; }
+        public string ImageUrl { get; set; }
+
 
         [NotMapped] //not maped does not in clude the field in to the DB
         public IFormFile File { get; set; }
 
-        public string ImageUrl { get; set; }
+        public ICollection<Reservation> Reservations { get; set; }
 
     }
 }
