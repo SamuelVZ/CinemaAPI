cbaf63a309e7a4c27525720e9c9f3e0d33dda6f3
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-08 17:50:21 -0800

    Put and delete mehods added

--- CinemaAPI/Controllers/MoviesController.cs
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -27,27 +27,30 @@ namespace CinemaAPI.Controllers {
         [HttpGet]
         [Route("{id}")]
         public IActionResult GetMovieById([FromRoute]int id) {
-            return Ok(_moviesService.GetMovieById(id));
+            var movie = _moviesService.GetMovieById(id);
+
+            return movie != null ? Ok(movie): NotFound("Movie with id " + id + " does not exist") ;
         }
 
         [HttpPost] 
         public IActionResult Add([FromBody]AddMovieDto addMovieDto) {
             var movie = _moviesService.AddMovie(addMovieDto);
-            return Ok(movie);
+            return Created("", movie);
         }
 
-        //[HttpPut]
-        //[Route("{id}")]
-        //public IActionResult Put([FromRoute]int id, [FromBody]Movie movie) {
-        //    movies[id] = movie;
-        //    return Ok(movies);
-        //}
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Put([FromRoute] int id, [FromBody] AddMovieDto movie) {
+            var updatedMovie = _moviesService.UpdateMovie(id, movie);
+            return updatedMovie != null ? Ok(updatedMovie) : NotFound("Movie with id " + id + " does not exist");
+        }
 
-        //[HttpDelete]
-        //[Route("{id}")]
-        //public IActionResult Delete([FromRoute]int id) {
-        //    movies.RemoveAt(id);
-        //    return Ok(movies);
-        //}
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id) {
+            var deleted = _moviesService.DeleteMovie(id);
+
+            return deleted ? Ok("Movie deleted") : NotFound("The movie tried to delete does not exist");
+        }
     }
 }
