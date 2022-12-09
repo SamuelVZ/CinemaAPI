b31403d9dadaf020aab5c3edfecda8c402edbb4a
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-09 07:02:22 -0800

    images included on the movie model

--- CinemaAPI/Models/Movie.cs
+++ CinemaAPI/Models/Movie.cs
@@ -1,7 +1,16 @@
-﻿namespace CinemaAPI.Models {
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CinemaAPI.Models {
     public class Movie {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Language { get; set; }
+        public double Rate { get; set; }
+
+        [NotMapped] //not maped does not in clude the field in to the DB
+        public IFormFile File { get; set; }
+
+        public string ImageUrl { get; set; }
+
     }
 }
