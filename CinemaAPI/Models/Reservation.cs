25ebf84f95fb77ff3a2478019f7457a0859fce8d
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-09 08:08:50 -0800

    reservation model created

---
+++ CinemaAPI/Models/Reservation.cs
@@ -0,0 +1,12 @@
+﻿namespace CinemaAPI.Models {
+    public class Reservation {
+        public int Id { get; set; }
+        public int Qty { get; set; }
+        public double Price { get; set; }
+        public string Phone { get; set; }
+        public DateTime ReservationTime { get; set; }
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
