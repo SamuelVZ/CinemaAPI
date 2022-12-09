2d8e30ca37e75541097f66092d82009de3f40e16
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-09 07:49:11 -0800

    Required tag for properties

--- CinemaAPI/Dto/AddMovieDto.cs
+++ CinemaAPI/Dto/AddMovieDto.cs
@@ -1,9 +1,14 @@
-﻿namespace CinemaAPI.Dto {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaAPI.Dto {
     public class AddMovieDto {
+        //[Required(ErrorMessage = "Name cannot be null")]
+        //no need for this on .net 6 fields since default is required unless you specify the prop can be nullable with "?"
+        //only use it if you want to display a personalkized messaje for empty values
         public string Name { get; set; }
         public string Language { get; set; }
         public double Rate { get; set; }
-        public IFormFile File { get; set; }
-        public string ImageUrl { get; set; }
+        public IFormFile? File { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
