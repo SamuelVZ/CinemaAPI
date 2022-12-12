7e79ff995df5694cba165eb183994ebed9fc0777
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-12 14:50:43 -0800

    DisplayMovies Dto added

---
+++ CinemaAPI/Dto/DisplayMoviesDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaAPI.Dto {
+    public class DisplayMoviesDto {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public string Language { get; set; }
+        public double Rating { get; set; }
+        public string Genre { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
