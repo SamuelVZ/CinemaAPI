3be21082bbb5d218c844cb18d8c6086ebe3a430a
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-11 18:12:57 -0800

    User Dtos added

---
+++ CinemaAPI/Dto/AddUserDto.cs
@@ -0,0 +1,13 @@
+﻿using CinemaAPI.Models;
+
+namespace CinemaAPI.Dto {
+    public class AddUserDto {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        //public string Role { get; set; }
+
+        //1 to many relationship
+        //public ICollection<Reservation> Reservations { get; set; }
+    }
+}
---
+++ CinemaAPI/Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using CinemaAPI.Models;
+
+namespace CinemaAPI.Dto {
+    public class UserDto {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+        //1 to many relationship
+        public ICollection<Reservation> Reservations { get; set; }
+    }
+}
