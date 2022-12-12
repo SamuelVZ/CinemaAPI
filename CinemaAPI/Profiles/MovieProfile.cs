c30c2d299c14e83b5c9c7219836a3dbba3109f1a
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-12 14:51:20 -0800

    map added for displaymoviesDto

--- CinemaAPI/Profiles/MovieProfile.cs
+++ CinemaAPI/Profiles/MovieProfile.cs
@@ -1,12 +1,15 @@
 ﻿using AutoMapper;
 using CinemaAPI.Dto;
 using CinemaAPI.Models;
+using System.Security.Cryptography.Xml;
 
 namespace CinemaAPI.Profiles {
     public class MovieProfile : Profile {
         public MovieProfile() {
             CreateMap<Movie, MovieDto>().ReverseMap();
             CreateMap<Movie, AddMovieDto>().ReverseMap();
+            CreateMap<Movie, DisplayMoviesDto>().ReverseMap();
+            //CreateMap<IQueryable, FindMovies>().ReverseMap();
         }
     }
 }
