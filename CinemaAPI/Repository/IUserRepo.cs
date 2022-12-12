6f69290e3f34d52fd2811b15c3946806e6e413ef
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-11 18:14:36 -0800

    user Repo added with register and check email

---
+++ CinemaAPI/Repository/IUserRepo.cs
@@ -0,0 +1,9 @@
+﻿using CinemaAPI.Models;
+
+namespace CinemaAPI.Repository {
+    public interface IUserRepo {
+        public Boolean CheckIfEmailExist(string email);
+        public User RegisterUser(User user);
+
+    }
+}
---
+++ CinemaAPI/Repository/UserRepo.cs
@@ -0,0 +1,32 @@
+﻿using CinemaAPI.Data;
+using CinemaAPI.Models;
+
+namespace CinemaAPI.Repository {
+    public class UserRepo : IUserRepo {
+        private readonly CinemaDbContext _cinemaDbContext;
+
+        public UserRepo(CinemaDbContext cinemaDbContext) {
+            this._cinemaDbContext = cinemaDbContext;
+        }
+
+        public User RegisterUser(User user) {
+            _cinemaDbContext.Users.Add(user);
+            _cinemaDbContext.SaveChanges();
+            return user;
+        }
+
+
+
+
+        public bool CheckIfEmailExist(string email) {
+
+            var userWithSameEmail = _cinemaDbContext.Users.Where( u => u.Email == email ).SingleOrDefault();
+
+            if (userWithSameEmail != null) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
