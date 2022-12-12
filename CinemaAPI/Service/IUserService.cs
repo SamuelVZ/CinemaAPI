542104083d29aac51679563db1032f7a3f78c3bc
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-11 18:15:18 -0800

    user service added with register and check email method

---
+++ CinemaAPI/Service/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using CinemaAPI.Dto;
+using CinemaAPI.Models;
+
+namespace CinemaAPI.Service {
+    public interface IUserService {
+
+        public UserDto RegisterUser(AddUserDto addUserDto);
+        public Boolean CheckIfEmailExist(string email);
+
+    }
+}
---
+++ CinemaAPI/Service/UserService.cs
@@ -0,0 +1,36 @@
+﻿using AuthenticationPlugin;
+using AutoMapper;
+using CinemaAPI.Dto;
+using CinemaAPI.Models;
+using CinemaAPI.Repository;
+
+namespace CinemaAPI.Service {
+    public class UserService : IUserService {
+        private readonly IUserRepo _userRepo;
+        private readonly IMapper _mapper;
+
+        public UserService(IUserRepo userRepo, IMapper mapper) {
+            this._userRepo = userRepo;
+            this._mapper = mapper;
+        }
+
+        public UserDto RegisterUser(AddUserDto addUserDto) {
+
+            User user = _mapper.Map<AddUserDto, User>(addUserDto);
+            user.Role = "User";
+            user.Password = SecurePasswordHasherHelper.Hash(addUserDto.Password);
+            var createdUser = _userRepo.RegisterUser(user);
+            var userDto = _mapper.Map<User, UserDto>(createdUser);
+
+            return userDto;
+        }
+
+
+
+
+        public bool CheckIfEmailExist(string email) {
+            return _userRepo.CheckIfEmailExist(email);
+        }
+
+    }
+}
