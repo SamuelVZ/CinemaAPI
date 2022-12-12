f6855ca35b307f0ab3f1da85cef416b3186a1f4e
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-11 18:13:57 -0800

    Profile added for user maping

---
+++ CinemaAPI/Profiles/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using CinemaAPI.Dto;
+using CinemaAPI.Models;
+
+namespace CinemaAPI.Profiles {
+    public class UserProfile : Profile {
+        public UserProfile() {
+            CreateMap<User, AddUserDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+        }
+    }
+}
