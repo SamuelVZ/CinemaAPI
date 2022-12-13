61db2ca81370297aa9b7a9f45941f89b19a9c3a6
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-12 18:51:34 -0800

    reservation service added with methods

---
+++ CinemaAPI/Service/IReservationsService.cs
@@ -0,0 +1,13 @@
+﻿using CinemaAPI.Dto;
+using CinemaAPI.Models;
+
+namespace CinemaAPI.Service {
+    public interface IReservationsService {
+
+        public Reservation AddResservation(Reservation reservation);
+        public IQueryable GetAllReservations();
+        public ReservationDetails GetReservationById(int id);
+        public Boolean deleteReservationById(int id);
+
+    }
+}
---
+++ CinemaAPI/Service/ReservationsService.cs
@@ -0,0 +1,30 @@
+﻿using CinemaAPI.Dto;
+using CinemaAPI.Models;
+using CinemaAPI.Repository;
+
+namespace CinemaAPI.Service {
+    public class ReservationsService : IReservationsService{
+        private readonly IReservationsRepo _reservationsRepo;
+
+        public ReservationsService(IReservationsRepo reservationsRepo) {
+            this._reservationsRepo = reservationsRepo;
+        }
+
+        public Reservation AddResservation(Reservation reservation) {
+            reservation.ReservationTime = DateTime.Now;
+            return _reservationsRepo.AddResservation(reservation);
+        }
+
+        public IQueryable GetAllReservations() {
+            return _reservationsRepo.GetAllReservations();
+        }
+
+        public ReservationDetails GetReservationById(int id) {
+            return _reservationsRepo.GetReservationById(id);
+        }
+        public bool deleteReservationById(int id) {
+            return _reservationsRepo.deleteReservationById(id);
+        }
+
+    }
+}
