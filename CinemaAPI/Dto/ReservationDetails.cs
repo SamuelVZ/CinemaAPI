4bcba093eb9fd57551aada86ccd2a7f0d71b570b
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-12 18:52:07 -0800

    reservationDetails added

---
+++ CinemaAPI/Dto/ReservationDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace CinemaAPI.Dto {
+    public class ReservationDetails {
+        public int Id { get; set; }
+        public int Qty { get; set; }
+        public double Price { get; set; }
+        public string Phone { get; set; }
+        public DateTime ReservationTime { get; set; }
+        public int CustomerName { get; set; }
+        public string MovieName { get; set; }
+        public string Email { get; set; }
+        public DateTime PlayingDate { get; set; }
+        public DateTime PlatingTime { get; set; }
+
+    }
+}
